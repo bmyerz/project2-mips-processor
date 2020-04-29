f0b23cb504467b2d9c696c614c27cf4ca0a1e241
Brandon Myers <brandon-d-myers@uiowa.edu>
2020-04-29 12:31:05 -0500

    sanitize logisim file

---
+++ sanitize.sh
@@ -0,0 +1,8 @@
+set -o nounset
+file=$1
+
+sed -i -e 's/file#.*[/]alu.circ/file#alu.circ/' $file
+sed -i -e 's/file#.*[/]mem.circ/file#mem.circ/' $file
+sed -i -e 's/file#.*[/]regfile.circ/file#regfile.circ/' $file
+sed -i -e 's/file#.*[/]control.circ/file#control.circ/' $file
+sed -i -e 's/file#.*[/]cpu.circ/file#cpu.circ/' $file
